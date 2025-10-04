62dde0e32003974dc3c2969327b107c16514fef0
Charles Harrison <35784256+notxcharles@users.noreply.github.com>
2025-10-04 21:07:39 +0100

    Update EggHuntCommands.java
    Command to view progress: /egghunt progress
    Requires permission: miniproject.egghunt.progress

--- src/main/java/me/candidate/miniproject/command/EggHuntCommands.java
+++ src/main/java/me/candidate/miniproject/command/EggHuntCommands.java
@@ -2,6 +2,11 @@ package me.candidate.miniproject.command;
 
 import me.candidate.miniproject.MiniProjectPlugin;
 import me.candidate.miniproject.scaffold.command.AnnotationCommand;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.command.CommandSender;
+import org.incendo.cloud.annotations.Command;
+import org.incendo.cloud.annotations.CommandDescription;
+import org.incendo.cloud.annotations.Permission;
 
 public class EggHuntCommands implements AnnotationCommand {
 
@@ -13,7 +18,16 @@ public class EggHuntCommands implements AnnotationCommand {
 
     // Your command implementation here
     // This should open the Progress UI
-    public void onEggHuntCommand() {
+    @Command("egghunt progress")
+    @Permission("miniproject.egghunt.progress") //luckperms user MarkDonovan permission set miniproject.egghunt.progress true
+    @CommandDescription("View current progress in the egghunt")
+    public void onEggHuntCommand(CommandSender sender) {
+        var parsedMessage = MiniMessage.miniMessage().deserialize("<#ff0000>user wants to view their egg hunt progress!");
+
+        // Send the message to the player
+        sender.sendMessage(parsedMessage);
+
+        // TODO
         // Open the Egg Hunt UI
     }
 
